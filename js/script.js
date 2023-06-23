e31bb9a07191f6e439964a5b7f2526a699eaa99a
E Luckie <eluckie.d@gmail.com>
2023-06-22 19:48:58 -0600

    update job duties & add achievements & events sections

--- js/script.js
+++ js/script.js
@@ -1,14 +1,47 @@
 function viewExp() {
   document.getElementById("experience").removeAttribute("class");
-  document.getElementById("view-btn").setAttribute("class", "hidden");
+  document.getElementById("experience-btn").setAttribute("class", "hidden");
 }
 
 function hideExp() {
   document.getElementById("experience").setAttribute("class", "hidden");
-  document.getElementById("view-btn").removeAttribute("class");
+  document.getElementById("experience-btn").removeAttribute("class");
+}
+
+function viewAch() {
+  document.getElementById("achievements").removeAttribute("class");
+  document.getElementById("achievements-btn").setAttribute("class", "hidden");
+}
+
+function hideAch() {
+  document.getElementById("achievements").setAttribute("class", "hidden");
+  document.getElementById("achievements-btn").removeAttribute("class");
+}
+
+function viewNso() {
+  document.getElementById("nso").removeAttribute("class");
+  document.getElementById("remodel").setAttribute("class", "hidden");
+  document.getElementById("fla").setAttribute("class", "hidden");
+}
+
+function viewRemodel() {
+  document.getElementById("remodel").removeAttribute("class");
+  document.getElementById("nso").setAttribute("class", "hidden");
+  document.getElementById("fla").setAttribute("class", "hidden");
+}
+
+function viewFla() {
+  document.getElementById("fla").removeAttribute("class");
+  document.getElementById("remodel").setAttribute("class", "hidden");
+  document.getElementById("nso").setAttribute("class", "hidden");
 }
 
 window.addEventListener("load", () => {
-  document.getElementById("view-btn").addEventListener("click", viewExp);
-  document.getElementById("hide-btn").addEventListener("click", hideExp);
+  document.getElementById("experience-btn").addEventListener("click", viewExp);
+  document.getElementById("hide-exp-btn").addEventListener("click", hideExp);
+  document.getElementById("achievements-btn").addEventListener("click", viewAch);
+  document.getElementById("hide-ach-btn").addEventListener("click", hideAch);
+  document.getElementById("nso-title").addEventListener("click", viewNso);
+  document.getElementById("remodel-title").addEventListener("click", viewRemodel);
+  document.getElementById("fla-title").addEventListener("click", viewFla);
 });
